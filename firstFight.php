a3a37c673bfd711a0e55cce5d6d0745f29662864
giuseppevignanello <peppe.vignanello@gmail.com>
2023-08-01 10:00:15 +0200

    add first Opponent

--- firstFight.php
+++ firstFight.php
@@ -8,4 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     require_once "Character.php";
     $hero = new Character($hero_name, 'playable', 100, 20, 10);
     var_dump($hero);
+
+
+    //create the first Opponent
+    require_once('Character.php');
+
+    $firstOpponent = new Character('FirstOpponent', 'opponent', 100, 10, 10);
+
+    var_dump($firstOpponent);
 }
