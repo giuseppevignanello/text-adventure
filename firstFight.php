d64ef942f1b73b2ec869ca91bcc9b29817e0daac
giuseppevignanello <peppe.vignanello@gmail.com>
2023-08-01 11:58:26 +0200

    fix first Fight logic

--- firstFight.php
+++ firstFight.php
@@ -1,54 +1,82 @@
 <?php
-
+require_once "Character.php";
 session_start();
 
-require_once "Character.php";
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // take hero name
-    $hero_name = $_POST["hero_name"];
+    // Check if the hero is saved correctly 
+    if (!isset($_SESSION["hero"])) {
+        // If there isn't an hero redirect user on the creation page
+        header("Location: heroCreation.php");
+        exit;
+    }
 
-    //create the hero character
-    require_once "Character.php";
-    $hero = new Character($hero_name, 'playable', 100, 20, 10);
+    // take the hero 
+    $hero = $_SESSION["hero"];
 
-
-    //create the first Opponent
-    require_once('Character.php');
-
+    // Create the first opponent
     $firstOpponent = new Character('FirstOpponent', 'opponent', 100, 10, 10);
 
+    // take hero data
 
-    // get Hero data
+    // take hero data
+    $hero_name = $hero->getCharacterData("name");
     $hero_attack = $hero->getCharacterData("attack");
     $hero_defense = $hero->getCharacterData("defense");
     $hero_life_points = $hero->getCharacterData('lifePoints');
 
-
-    // get opponent data
-
+    // take opponent data
     $firstOpponent_name = $firstOpponent->getCharacterData('name');
     $firstOpponent_attack = $firstOpponent->getCharacterData('attack');
     $firstOpponent_defense = $firstOpponent->getCharacterData('defense');
     $firstOpponent_lifePoints = $firstOpponent->getCharacterData('lifePoints');
 
-
-    // add fighting logic
-
+    // update data after fight
     if ($hero_attack > $firstOpponent_defense) {
-        $damageDealt = $hero_attack;
+        $damageDealt = $hero_attack - $firstOpponent_defense;
         $firstOpponent_lifePoints -= $damageDealt;
-        echo ("Nice shot $hero_name: $firstOpponent_name lost $hero_attack life points");
-    } else if ($hero_attack < $firstOpponent_defense) {
+        $result_message = "Nice shot $hero_name: $firstOpponent_name lost $damageDealt life points";
+    } else if ($hero_attack <= $firstOpponent_defense) {
         $damageTaken = $firstOpponent_defense - $hero_attack;
         $hero_life_points -= $damageTaken;
-        echo ("oh no, $firstOpponent_name is stronger than you. The attack backfired, you lost $hero_attack life points");
+        $result_message = "Oh no, $firstOpponent_name is stronger than you. The attack backfired, you lost $damageTaken life points";
     } else {
-        echo ("tie");
+        $result_message = "It's a tie";
     }
 
-    // update the characters data
+    // updating
     $hero->updateCharacterData('lifePoints', $hero_life_points);
     $firstOpponent->updateCharacterData('lifePoints', $firstOpponent_lifePoints);
-    $_SESSION["avversario"] = $firstOpponent;
+
+    // save the opponent in the session
+    $_SESSION["opponent"] = $firstOpponent;
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Fight</title>
+</head>
+
+<body class="bg-success">
+    <div class="container">
+        <?php if (isset($result_message)) : ?>
+        <div class="text-white m-2">
+            <h4><?php echo $result_message; ?></h4>
+        </div>
+        <?php endif; ?>
+    </div>
+</body>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
+
+</html>
---
+++ heroCreation.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // take hero name
+    $hero_name = $_POST["hero_name"];
+
+    // create hero character
+    require_once "Character.php";
+    $hero = new Character($hero_name, 'playable', 100, 20, 10);
+
+    // save the hero in the session
+    $_SESSION["hero"] = $hero;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>First Fight</title>
+</head>
+
+<body class="bg-success text-white">
+    <div class="container mt-4">
+        <form method="post" action="firstFight.php">
+            <h4>Enjoy your first fight</h4>
+            <button class="btn btn-primary" type="submit">Fight</button>
+        </form>
+    </div>
+</body>
+
+</html>
