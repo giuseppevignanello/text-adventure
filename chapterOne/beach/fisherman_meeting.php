4d72a5f67126b43d9b977d1a44c831a772b7f6e8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-08-04 09:31:56 +0200

    add fisherman and dog meeting scene

--- chapterOne/beach/fisherman_meeting.php
+++ chapterOne/beach/fisherman_meeting.php
@@ -9,10 +9,27 @@ $hero_inventory = $hero->getInventory();
 <?php include_once '../../partials/header.php' ?>
 <div class="container">
     <?php include_once '../../partials/stats.php' ?>
-    <h2>Fisherman</h2>
-    <form method="post" action="../firstSetting.php">
-        <h4>Move: </h4>
-        <button class="hacking_green_bg border-0" type="submit">Come back</button>
-    </form>
+</div>
+<div id="fisherman_image">
+</div>
+<div class="container">
+    <p class="mt-2">The gentle lapping of the ocean waves provides a soothing rhythm, while the sun's golden rays create
+        a warm and
+        serene atmosphere. Amidst this coastal serenity stands an elderly fisherman, hunched over his fishing rod. With
+        a weathered face and eyes that seem to hold a lifetime of wisdom, he appears content in his solitude, giving off
+        an aura of not wanting to be disturbed. The rhythmic sound of seagulls adds to the peaceful ambiance, their
+        graceful flight complementing the tranquility of the scene. </p>
+    <div class="d-flex gap-2">
+        <div>
+            <form method="post" action="../beach/beachStart.php">
+                <button class="hacking_green_bg border-0" type="submit">Come back</button>
+            </form>
+        </div>
+        <div>
+            <form action="fisherman_dialogue.php" method="post">
+                <button class="hacking_green_bg border-0" type="submit">Talk with him</button>
+            </form>
+        </div>
+    </div>
 </div>
 <?php include_once '../../partials/footer.php' ?>
--- chapterOne/beach/stray_dog_meeting.php
+++ chapterOne/beach/stray_dog_meeting.php
@@ -9,10 +9,26 @@ $hero_inventory = $hero->getInventory();
 <?php include_once '../../partials/header.php' ?>
 <div class="container">
     <?php include_once '../../partials/stats.php' ?>
-    <h2>Stray Dog</h2>
-    <form method="post" action="../firstSetting.php">
-        <h4>Move: </h4>
-        <button class="hacking_green_bg border-0" type="submit">Come back</button>
-    </form>
+</div>
+<div id="dog_image">
+
+</div>
+<div class="container">
+    <p class="mt-2"></p>You come across a lone figure standing before you – a stray dog. Its shaggy fur ripples in the
+    breeze, and its
+    warm, brown eyes hold a mix of weariness and curiosity. There is a certain air of independence about this canine
+    wanderer, as if it has grown accustomed to the freedom of roaming on this beach. </p>
+    <div class="d-flex gap-2">
+        <div>
+            <form method="post" action="../beach/beachStart.php">
+                <button class="hacking_green_bg border-0" type="submit">Come back</button>
+            </form>
+        </div>
+        <div>
+            <form action="dog_fight.php" method="post">
+                <button class="hacking_green_bg border-0" type="submit">Get close to him</button>
+            </form>
+        </div>
+    </div>
 </div>
 <?php include_once '../../partials/footer.php' ?>
